fd44cb4f8d444d1edbe204fcd9eb22c03ed1c7a2
Sooyeon  Kim <71349348+SaraKim-sy@users.noreply.github.com>
2020-10-13 11:32:00 -0400

    complete
    complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,4 @@
+# Modules
 import os
 import csv
 
@@ -10,22 +11,27 @@ output_path = os.path.join('PyBank', 'Analysis', 'financial_analysis.txt')
 # Read in the CSV file
 with open(file_path) as file:
     csvreader = csv.reader(file)
-    next(csvreader, None)
+
+    # Store the header row
+    csv_header = next(file)
+    
+    # Store the first row and set it as the previous row to use it for calculating changes for each month
     first_row = next(csvreader)
     previous_row = first_row
 
+    # Set variable to count the total number of months & add 1 to it for the first month
     total_months = 0
     total_months += 1
 
+    # Set variable to calculate the net total amount of Profit/Losses & add net profit/loss of the first month 
     net_total_profit_losses = 0
     net_total_profit_losses += int(first_row[1])
     
-    # Creat a list to store changes
-    changes = []
+    # Set variable to calculate changes for each month
     change = 0
 
-    # Creat a dictionary to store changes : dates
-    changes_dates_dict = {}
+    # Creat a dictionary to store dates(key) and change(value)
+    date_change_dict = {}
     
 
     for row in csvreader:
@@ -34,21 +40,30 @@ with open(file_path) as file:
 
         # Add the amount of profit/losses for each month to calculate the net total over the entire period
         net_total_profit_losses += int(row[1])
+
+        # Calculate changes for each month 
         change = int(row[1]) - int(previous_row[1])
-        changes.append(change)
-        changes_dates_dict[change] = row[0]
+        
+        # Store the date and the change in the dictionary
+        date_change_dict[row[0]] = change
+
+        # Set the current row as the previous row for next month
         previous_row = row
 
     # Calculate the average of the changes in "Profit/Losses" over the entire period
+    changes = list(date_change_dict.values())
     average_changes = round(sum(changes) / len(changes), 2)
 
+    # Sort the dictionary by value(change amount) in descending order
+    date_change_sorted = sorted(date_change_dict.items(), key=lambda x:x[1], reverse=True)
+
     # Find the greatest increase in profits (date and amount) over the entire period
-    greatest_increase_in_profits_amt = max(changes)
-    greatest_increase_in_profits_date = changes_dates_dict[greatest_increase_in_profits_amt]
+    greatest_increase_amt = date_change_sorted[0][1]
+    greatest_increase_date = date_change_sorted[0][0]
 
     # Find the greatest decrease in losses (date and amount) over the entire period
-    greatest_decrease_in_profits_amt = min(changes)
-    greatest_decrease_in_profits_date = changes_dates_dict[greatest_decrease_in_profits_amt]
+    greatest_decrease_amt = date_change_sorted[-1][1]
+    greatest_decrease_date = date_change_sorted[-1][0]
 
     # Print out the Financial Analysis to the terminal
     print("Financial Analysis")
@@ -56,8 +71,9 @@ with open(file_path) as file:
     print(f"Total Months: {total_months}")
     print(f"Total: ${net_total_profit_losses}")
     print(f"Average Change: ${average_changes}")
-    print(f"Greatest Increase in Profits: {greatest_increase_in_profits_date} (${greatest_increase_in_profits_amt})")
-    print(f"Greatest Decrease in Profits: {greatest_decrease_in_profits_date} (${greatest_decrease_in_profits_amt})")
+    print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amt})")
+    print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amt})")
+
 
     # Export a text file with the results
     with open(output_path, 'w') as outputfile:
@@ -66,5 +82,5 @@ with open(file_path) as file:
         outputfile.write(f"Total Months: {total_months}\n")
         outputfile.write(f"Total: ${net_total_profit_losses}\n")
         outputfile.write(f"Average Change: ${average_changes}\n")
-        outputfile.write(f"Greatest Increase in Profits: {greatest_increase_in_profits_date} (${greatest_increase_in_profits_amt})\n")
-        outputfile.write(f"Greatest Decrease in Profits: {greatest_decrease_in_profits_date} (${greatest_decrease_in_profits_amt})\n")
+        outputfile.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amt})\n")
+        outputfile.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amt})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,4 @@
+# Modules
 import os
 import csv
 
@@ -8,40 +9,67 @@ file_path = os.path.join('PyPoll', 'Resources', 'election_data.csv')
 output_path = os.path.join('PyPoll', 'Analysis', 'election_results.txt')
 
 # Read in the CSV file
-with open(file_path) as file:
+with open(file_path, 'r') as file:
     csvreader = csv.reader(file)
-    next(csvreader, None)
-    first_row = next(csvreader)
-
+    # Store the header row
+    csv_header = next(file)
+    
+    # Set variable to count the total number of votes cast
     total_votes = 0
 
+    # List and Dictionary to store data
     candidates_voted = []
-
     candidates_dict = {}
 
     for row in csvreader:
         # Add 1 every row to calculate the total number of votes cast
         total_votes = total_votes + 1
+
+        # Store all the candidates each voter voted in the list
         candidates_voted.append(row[2])
-        
+
+    # Count the total number of votes each candidate won
     for candidate in candidates_voted:
         if candidate not in candidates_dict:
             candidates_dict[candidate] = 1
         else:
             candidates_dict[candidate] += 1
 
-    candidates_dict_sorted = sorted(candidates_dict.items(), key=lambda x:x[1], reverse=True)
-    winner = list(sorted(candidates_dict.items(), key=lambda x:x[1], reverse=True)[0])[0]
+    # Sort the dictionary by value(the number of votes) in descending order
+    candidates_sorted = sorted(candidates_dict.items(), key=lambda x:x[1], reverse=True)
 
-    print(candidates_dict_sorted)
-'''
+    # Find the winner
+    winner = candidates_sorted[0][0]
+
+# Print out the Election Results to the terminal
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
 print("-------------------------")
-for candidate in candidates_dict_sorted:
-    print(f"{candidate}: {round(candidates_dict_sorted[candidate]/total_votes*100, 5)}% ({candidates_dict_sorted[candidate]})")
+for i in range(len(candidates_sorted)):
+    # Each candidate's name 
+    name = candidates_sorted[i][0]
+    # The total number of votes each candidate won
+    votes_num = candidates_sorted[i][1]
+    # Calculate the percentage of votes each candidate won
+    votes_percent = round(votes_num/total_votes*100, 3)
+    print(f"{name}: {votes_percent}% ({votes_num})")
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
-'''
+
+
+# Export a text file with the results
+with open(output_path, 'w') as outputfile:
+    outputfile.write("Election Results\n")
+    outputfile.write("-------------------------\n")
+    outputfile.write(f"Total Votes: {total_votes}\n")
+    outputfile.write("-------------------------\n")
+    for i in range(len(candidates_sorted)):
+        name = candidates_sorted[i][0]
+        votes_num = candidates_sorted[i][1]
+        votes_percent = round(votes_num/total_votes*100, 3)
+        outputfile.write(f"{name}: {votes_percent}% ({votes_num})\n")
+    outputfile.write("-------------------------\n")
+    outputfile.write(f"Winner: {winner}\n")
+    outputfile.write("-------------------------\n")
